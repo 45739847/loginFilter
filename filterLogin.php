4cbf93fbc2dfa4d91ec8b4d88f4cc6ec30604349
Okami Studio <58578838+OkamiStudioMks@users.noreply.github.com>
2021-03-21 00:41:43 -0300

    Add files via upload

---
+++ filterLogin.php
@@ -0,0 +1,16 @@
+<?php
+function limpar_string($string) {
+    if($string !== mb_convert_encoding(mb_convert_encoding($string, 'UTF-32', 'UTF-8'), 'UTF-8', 'UTF-32'))
+    $string = mb_convert_encoding($string, 'UTF-8', mb_detect_encoding($string));
+    $string = htmlentities($string, ENT_NOQUOTES, 'UTF-8');
+    $string = preg_replace('`&([a-z]{1,2})(acute|uml|circ|grave|ring|cedil|slash|tilde|caron|lig);`i', '\1', $string);
+    $string = html_entity_decode($string, ENT_NOQUOTES, 'UTF-8');
+    $string = preg_replace(array('`[^a-z0-9]`i','`[-]+`'), ' ', $string);
+    $string = preg_replace('/( ){2,}/', '$1', $string);
+    return $string;
+}
+
+$username = limpar_string($_POST["user"]);
+$senha = limpar_string($_POST["pass"]);
+
+?>
